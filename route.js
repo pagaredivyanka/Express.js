36b08d3bd6bc7d0570816a142fe5bf92c46aab87
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-01-20 23:40:18 +0530

    Update route.js

--- route.js
+++ route.js
@@ -4,5 +4,13 @@ const route = express.Router()
 
 route.get('/findall',studecontroller.showdetail)
 
+route.post('/register',studecontroller.adduser)
+
+route.put('/update/:fname',studecontroller.updatedata)
+
+route.delete('/delete/:fname',studecontroller.deleteuser)
+
+route.post('/login/:fname',studecontroller.login)
+
 
 module.exports = route
